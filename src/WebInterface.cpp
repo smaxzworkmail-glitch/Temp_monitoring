be346d8d5fa38515e6c6be9463eb46503abc290e
Fabler <smaxz.workmail@gmail.com>
2026-01-27 03:30:43 +0200

    pre new index

--- src/WebInterface.cpp
+++ src/WebInterface.cpp
@@ -1,6 +1,7 @@
 #include "WebInterface.h"
 #include <WiFi.h>
 #include <LittleFS.h>
+#include <ArduinoJson.h>
 
 AsyncWebServer server(80);
 
@@ -26,9 +27,26 @@ void initWebInterface() {
         JsonDocument doc;
         auto& sensors = getSensors();
         
-        for (size_t i = 0; i < sensors.size(); i++) {
-            doc[i]["name"] = sensors[i].name;
-            doc[i]["temp"] = sensors[i].currentTemp;
+        for (auto &s : sensors) {
+            JsonObject sObj = doc.add<JsonObject>();
+            sObj["name"] = s.name;
+            sObj["current"] = s.currentTemp;
+            
+            // Збираємо годинні дані
+            JsonArray hArr = sObj["hour"].to<JsonArray>();
+            int hStart = s.history.hourFull ? s.history.hourIdx : 0;
+            int hCount = s.history.hourFull ? MAX_HOUR_POINTS : s.history.hourIdx;
+            for (int i = 0; i < hCount; i++) {
+                hArr.add(s.history.hourData[(hStart + i) % MAX_HOUR_POINTS]);
+            }
+            
+            // Збираємо добові дані
+            JsonArray dArr = sObj["day"].to<JsonArray>();
+            int dStart = s.history.dayFull ? s.history.dayIdx : 0;
+            int dCount = s.history.dayFull ? MAX_DAY_POINTS : s.history.dayIdx;
+            for (int i = 0; i < dCount; i++) {
+                dArr.add(s.history.dayData[(dStart + i) % MAX_DAY_POINTS]);
+            }
         }
         
         String response;
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include "Storage.h"
 #include "WebInterface.h" // 1. Додали новий модуль
 
+// Змінні для відстеження часу (таймери)
+unsigned long lastMinuteTick = 0;
+unsigned long last15MinTick = 0;
+
 void setup() {
     Serial.begin(115200);
 
@@ -23,6 +27,22 @@ void setup() {
 
 void loop() {
     updateTemperatures();
+    
+    unsigned long currentMillis = millis();
+
+    // 2. Таймер: кожну хвилину (60 000 мс)
+    if (currentMillis - lastMinuteTick >= 60000) {
+        addHourPoint(); // Записуємо в масив "Остання година"
+        lastMinuteTick = currentMillis;
+        Serial.println("Точка додана в годинний графік");
+    }
+
+    // 3. Таймер: кожні 15 хвилин (900 000 мс)
+    if (currentMillis - last15MinTick >= 900000) {
+        addDayPoint(); // Записуємо в масив "Доба"
+        last15MinTick = currentMillis;
+        Serial.println("Точка додана в добовий графік");
+    }
     
     // Вивід у Serial залишаємо для контролю, 
     // але тепер дані будуть доступні і через браузер
